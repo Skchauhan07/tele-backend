5cce0f17e4329ff0f97a2a63781792abc0c077e7
akashanand842 <akashanand842@gmail.com>
2023-03-25 15:39:02 +0530

    normal

--- teleconsultation/src/main/java/com/teleconsultation/Repository/PatientRepository.java
+++ teleconsultation/src/main/java/com/teleconsultation/Repository/PatientRepository.java
@@ -1,5 +1,6 @@
 package com.teleconsultation.Repository;
 
+import com.teleconsultation.Entity.Doctor;
 import com.teleconsultation.Entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
